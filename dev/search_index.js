65356cb7707b1c930ef4c8a9c98665c4a8aba243
Documenter.jl <documenter@juliadocs.github.io>
2022-08-06 08:01:24 +0000

    build based on b9d47c8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = findroot","category":"page"},{"location":"#findroot","page":"Home","title":"findroot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for findroot.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [findroot]","category":"page"}]
+}
